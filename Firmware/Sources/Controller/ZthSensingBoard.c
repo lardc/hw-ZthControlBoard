8f32194a3b94455d1908b39965c9b01f8456cf4d
d.ivanov <d.ivanov@proton-electrotex.com>
2021-08-30 12:17:48 +0300

    Добавлена библиотека чтения измеренных данных с помощью ЭМ ZthSensingBoard

---
+++ Firmware/Sources/Controller/ZthSensingBoard.c
@@ -0,0 +1,57 @@
+// Header
+//
+#include "ZthSensingBoard.h"
+
+// Include
+//
+#include "ZbGPIO.h"
+#include "ZbSPI.h"
+#include "Global.h"
+#include "ConvertUtils.h"
+
+// Functions
+//
+Int16U ZthSB_ReadTcase1()
+{
+	return CONVERT_Tcase1(ZbSPIB_Read(&ZbGPIO_SB_Tc1_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadTcase2()
+{
+	return CONVERT_Tcase2(ZbSPIB_Read(&ZbGPIO_SB_Tc2_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadTcool1()
+{
+	return CONVERT_Tcool1(ZbSPID_Read(&ZbGPIO_SB_Tcool1_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadTcool2()
+{
+	return CONVERT_Tcool2(ZbSPIA_Read(&ZbGPIO_SB_Tcool2_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadTSP()
+{
+	return CONVERT_TSP(ZbSPIA_Read(&ZbGPIO_SB_TSP_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadIh()
+{
+	return CONVERT_Ih(ZbSPIB_Read(&ZbGPIO_SB_Ih_CS));
+}
+// ----------------------------------------
+
+Int16U ZthSB_ReadIm()
+{
+	return CONVERT_Im(ZbSPIB_Read(&ZbGPIO_SB_Im_CS));
+}
+// ----------------------------------------
+
+
+
---
+++ Firmware/Sources/Controller/ZthSensingBoard.h
@@ -0,0 +1,18 @@
+#ifndef ZTHSENSINGBOARD_H_
+#define ZTHSENSINGBOARD_H_
+
+// Include
+//
+#include "ZwBase.h"
+
+// Functions
+//
+Int16U ZthSB_ReadTcase1();
+Int16U ZthSB_ReadTcase2();
+Int16U ZthSB_ReadTcool1();
+Int16U ZthSB_ReadTcool2();
+Int16U ZthSB_ReadTSP();
+Int16U ZthSB_ReadIh();
+Int16U ZthSB_ReadIm();
+
+#endif /* ZTHSENSINGBOARD_H_ */
