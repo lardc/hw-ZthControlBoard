7fc99e125a968243141e27d20fe8b8a3aac99f18
d.ivanov <d.ivanov@proton-electrotex.com>
2021-08-30 15:13:00 +0300

    Добавлены математические библиотеки

---
+++ Firmware/Sources/Controller/IQmathUtils.c
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------
+// IQ Math utils
+// ----------------------------------------
+
+// Header
+#include "IQmathUtils.h"
+
+
+// Functions
+//
+#ifdef BOOT_FROM_FLASH_IQ16log
+	#pragma CODE_SECTION(_IQ16log, "ramfuncs");
+#endif
+_iq16 _IQ16log(_iq16 x)
+{
+	Int32S t, y;
+
+	y=0xa65af;
+	if(x<0x00008000) x<<=16,              y-=0xb1721;
+	if(x<0x00800000) x<<= 8,              y-=0x58b91;
+	if(x<0x08000000) x<<= 4,              y-=0x2c5c8;
+	if(x<0x20000000) x<<= 2,              y-=0x162e4;
+	if(x<0x40000000) x<<= 1,              y-=0x0b172;
+	t=x+(x>>1); if((t&0x80000000)==0) x=t,y-=0x067cd;
+	t=x+(x>>2); if((t&0x80000000)==0) x=t,y-=0x03920;
+	t=x+(x>>3); if((t&0x80000000)==0) x=t,y-=0x01e27;
+	t=x+(x>>4); if((t&0x80000000)==0) x=t,y-=0x00f85;
+	t=x+(x>>5); if((t&0x80000000)==0) x=t,y-=0x007e1;
+	t=x+(x>>6); if((t&0x80000000)==0) x=t,y-=0x003f8;
+	t=x+(x>>7); if((t&0x80000000)==0) x=t,y-=0x001fe;
+	x=0x80000000-x;
+	y-=x>>15;
+
+	return y;
+}
+
+// No more
---
+++ Firmware/Sources/Controller/IQmathUtils.h
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------
+// IQ Math utils
+// ----------------------------------------
+
+#ifndef __IQMATH_UTIL_H
+#define __IQMATH_UTIL_H
+
+// Include
+#include "stdinc.h"
+//
+#include "IQmathLib.h"
+
+
+// Types
+//
+typedef struct __FPair
+{
+	Int16S N, D;
+} FPair;
+
+
+// Macro
+//
+//
+#define IQ_INVOKE(iqn, func)	_IQ##iqn##func
+#define _IQI(A)					_IQmpyI32(_IQ(1), A)
+#define _IQIn(A, N)				(IQ_INVOKE(N, mpyI32)(_IQ##N(1), A))
+
+
+// Functions
+//
+// Calculate log of number
+_iq16 _IQ16log(_iq16 x);
+// Convert IQ number to FP representation form
+inline FPair _IQtoFP(_iq x);
+
+
+// Inline function
+//
+// Convert FP form of number to IQ number
+inline _iq _FPtoIQ(FPair fp)
+{
+	return _IQdiv(_IQI(fp.N), _IQI(fp.D));
+}
+//
+// Convert FP form of number to IQ number
+inline _iq _FPtoIQ2(Int16S N, Int16S D)
+{
+	return _IQdiv(_IQI(N), _IQI(D));
+}
+
+#endif // __IQMATH_UTIL_H
